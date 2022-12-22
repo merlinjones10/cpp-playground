6ea534cfae740a54e604195c4275c352b9d633ec
merlinjones10 <78985319+merlinjones10@users.noreply.github.com>
2022-12-22 11:04:50 +0000

    Initial Commit

---
+++ cpp-playground/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp-playground
+//
+//  Created by Merlin Jones on 22/12/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
